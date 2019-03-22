42919f5bc31816d9b7d712a3f07072d46b84d139
KieranVieira <kieranvieira@live.com>
2019-03-22 13:34:58 -0400

    Added analytics report to main dashboard based on selected offer

--- src/containers/dashboard/analytics/Analytics.js
+++ src/containers/dashboard/analytics/Analytics.js
@@ -1,31 +1,13 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux';
-
-import { getOfferAnalytics } from '../../../store/actions/analyticsAction.js';
 
 class Analytics extends Component {
-  componentDidMount(){
-    this.props.getOfferAnalytics(this.props.currentOffer)
-  }
-
   render() {
     return (
       <div>
-        <h1>{Object.entries(this.props.currentOffer).length && this.props.currentOffer}</h1>
       </div>
     )
   }
 }
 
-const mapStateToProps = state => {
-  return{
-    offerAnalytics: state.analyticsReducer.offerAnalytics
-  }
-}
 
-export default connect(
-  mapStateToProps,
-  {
-    getOfferAnalytics
-  }
-)(Analytics);
+export default Analytics;
