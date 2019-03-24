ca30358794cc60572e5529be112899fe528f051d
john2796 <jbmiranda22796@gmail.com>
2019-03-23 23:20:35 -0700

    changed path for tabs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 import './index.css';
 import App from './App';
