3823f6a018fdaa64cbed23a423550c19ff2bdc5d
KieranVieira <kieranvieira@live.com>
2019-03-24 01:44:42 -0400

    Added buttons to select ad size

--- src/components/ad-generator/forms/AdForm.js
+++ src/components/ad-generator/forms/AdForm.js
@@ -4,113 +4,146 @@ import { Form } from "./formStyles.js";
 
 const AdForm = props => {
   return (
-    <Form onSubmit={props.createAd}>
-      <div>
-        <h2>Create your ad.</h2>
+    <>
+      <button 
+        value="horizontal_banner"
+        name="size"
+        onClick={props.handleChange}
+      >
+      Horizontal banner
+      </button>
 
-        <label htmlFor="offer_id" />
-        <select
-          type="text"
-          placeholder="Offer"
-          name="offer_id"
-          value={props.productData.offer_id}
-          onChange={props.handleChange}
-        >
-          <option value="" disabled>
-            Select an offer
-          </option>
-          {props.offers.map(offer => {
-            return <option key={offer.id} value={offer.id}>{offer.name}</option>;
-          })}
-        </select>
+      <button 
+        value="vertical_banner"
+        name="size"
+        onClick={props.handleChange}
+      >
+      Vertical banner
+      </button>
 
-        <label htmlFor="headline" />
-        <input
-          type="text"
-          placeholder="Headline"
-          name="headline"
-          value={props.productData.headline}
-          onChange={props.handleChange}
-        />
+      <button 
+        value="square_banner"
+        name="size"
+        onClick={props.handleChange}
+      >
+      Square banner
+      </button>
 
-        <label htmlFor="tagline" />
-        <input
-          type="text"
-          placeholder="Tagline"
-          name="tagline"
-          value={props.productData.tagline}
-          onChange={props.handleChange}
-        />
+      <button 
+        value="plain_image"
+        name="size"
+        onClick={props.handleChange}
+      >
+      Plain Image Banner
+      </button>
+      <Form onSubmit={props.createAd}>
+        <div>
+          <h2>Create your ad.</h2>
 
-        <label htmlFor="message" />
-        <input
-          type="text"
-          placeholder="Message"
-          name="message"
-          value={props.productData.message}
-          onChange={props.handleChange}
-        />
+          <label htmlFor="offer_id" />
+          <select
+            type="text"
+            placeholder="Offer"
+            name="offer_id"
+            value={props.productData.offer_id}
+            onChange={props.handleChange}
+          >
+            <option value="" disabled>
+              Select an offer
+            </option>
+            {props.offers.map(offer => {
+              return <option key={offer.id} value={offer.id}>{offer.name}</option>;
+            })}
+          </select>
 
-        <label htmlFor="text_color" />
-        <input
-          type="text"
-          placeholder="Text Color"
-          name="text_color"
-          value={props.productData.text_color}
-          onChange={props.handleChange}
-        />
+          <label htmlFor="headline" />
+          <input
+            type="text"
+            placeholder="Headline"
+            name="headline"
+            value={props.productData.headline}
+            onChange={props.handleChange}
+          />
 
-        <label htmlFor="cta_button" />
-        <input
-          type="text"
-          placeholder="CTA Button"
-          name="cta_button"
-          value={props.productData.cta_button}
-          onChange={props.handleChange}
-        />
+          <label htmlFor="tagline" />
+          <input
+            type="text"
+            placeholder="Tagline"
+            name="tagline"
+            value={props.productData.tagline}
+            onChange={props.handleChange}
+          />
 
-        <label htmlFor="btn_color" />
-        <input
-          type="text"
-          placeholder="Button Color"
-          name="btn_color"
-          value={props.productData.btn_color}
-          onChange={props.handleChange}
-        />
+          <label htmlFor="message" />
+          <input
+            type="text"
+            placeholder="Message"
+            name="message"
+            value={props.productData.message}
+            onChange={props.handleChange}
+          />
 
-        <label htmlFor="btn_text_color" />
-        <input
-          type="text"
-          placeholder="Button Text Color"
-          name="btn_text_color"
-          value={props.productData.btn_text_color}
-          onChange={props.handleChange}
-        />
+          <label htmlFor="text_color" />
+          <input
+            type="text"
+            placeholder="Text Color"
+            name="text_color"
+            value={props.productData.text_color}
+            onChange={props.handleChange}
+          />
 
-        <label htmlFor="destination_url" />
-        <input
-          type="text"
-          placeholder="Destination Url"
-          name="destination_url"
-          value={props.productData.destination_url}
-          onChange={props.handleChange}
-        />
+          <label htmlFor="cta_button" />
+          <input
+            type="text"
+            placeholder="CTA Button"
+            name="cta_button"
+            value={props.productData.cta_button}
+            onChange={props.handleChange}
+          />
 
-        <label htmlFor="back_img" />
-        {/* --------------------- image upload --------------------- */}
-        <input
-          accept="image/*"
-          type="file"
-          placeholder="Background Image"
-          name="back_img"
-          multiple
-          onChange={props.handleFileChange}
-        />
-        {/* --------------------- image upload --------------------- */}
+          <label htmlFor="btn_color" />
+          <input
+            type="text"
+            placeholder="Button Color"
+            name="btn_color"
+            value={props.productData.btn_color}
+            onChange={props.handleChange}
+          />
 
-        <button type="submit">Create Ad</button>
-      </div>
-    </Form>
+          <label htmlFor="btn_text_color" />
+          <input
+            type="text"
+            placeholder="Button Text Color"
+            name="btn_text_color"
+            value={props.productData.btn_text_color}
+            onChange={props.handleChange}
+          />
+
+          <label htmlFor="destination_url" />
+          <input
+            type="text"
+            placeholder="Destination Url"
+            name="destination_url"
+            value={props.productData.destination_url}
+            onChange={props.handleChange}
+          />
+
+          <label htmlFor="back_img" />
+          {/* --------------------- image upload --------------------- */}
+          <input
+            accept="image/*"
+            type="file"
+            placeholder="Background Image"
+            name="back_img"
+            multiple
+            onChange={props.handleFileChange}
+          />
+          {/* --------------------- image upload --------------------- */}
+
+          <button type="submit">Create Ad</button>
+        </div>
+      </Form>
+    </>
   );
 };
 
