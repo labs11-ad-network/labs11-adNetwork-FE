2aa16d0430ef6f9324c50746a1629ffe9082dade
KieranVieira <kieranvieira@live.com>
2019-03-26 12:46:25 -0400

    removed auth0 login from dashboard

--- src/containers/auth-zero/Auth/auth0-variables.js
+++ src/containers/auth-zero/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: "ladnetwork.auth0.com",
   clientId: "XQh6ZLLicvNOdZUMBrCvmL2zyrgmE1fB",
-  callbackUrl: "https://kieranlabs.netlify.com/callback"
+  callbackUrl: "http://kieranlabs.netlify.com/callback"
 };
