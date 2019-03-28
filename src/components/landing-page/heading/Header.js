240518373dd9017108e4104dc1947ff11f7a9d6d
kieranvieira <kieranvieira@live.com>
2019-03-28 02:15:48 -0400

    Added curve to landing page

--- src/components/landing-page/heading/Header.js
+++ src/components/landing-page/heading/Header.js
@@ -1,17 +1,28 @@
 import React from 'react'
 import styled from 'styled-components';
 import Graphs from '../../../assets/graphs.png';
+import Curve from '../../../assets/curve.svg'
 
 const HeaderContainer = styled.div`
     background-color: #0A88DC;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    .main{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 70%;
+        height: 85vh;
+        margin: 0 auto;
+    }
+`;
+
+const CurveComponent = styled.img`
+    width: 100%;
+    height: auto;    
+    margin-top: -1px;
 `;
 
 const LeftSection = styled.div`
     color: #FFFFFF;
-    width: 35%;
     h1{
         font-size: 5.5rem;
         font-weight: 800;
@@ -32,22 +43,28 @@ const LeftSection = styled.div`
 
 const RightSection = styled.div`
     img{
-        margin-top: 130px;
+        width: 110%;
+        height: auto
     }
 `;
 
 const Header = () => {
   return (
-    <HeaderContainer>
-        <LeftSection>
-            <h1>Creepy Ads</h1>
-            <p>We are a non creepy ad network that presents itself as actually very creepy.</p>
-            <div/>
-        </LeftSection>
-        <RightSection>
-            <img src={Graphs} alt="" draggable={false}/>
-        </RightSection>
-    </HeaderContainer>
+    <>
+        <HeaderContainer>
+            <div className="main">
+                <LeftSection>
+                    <h1>Creepy Ads</h1>
+                    <p>We are a non creepy ad network that presents itself as actually very creepy.</p>
+                    <div/>
+                </LeftSection>
+                <RightSection>
+                    <img src={Graphs} alt="" draggable={false}/>
+                </RightSection>
+            </div>
+        </HeaderContainer>
+        <CurveComponent src={Curve} alt="" draggable={false} className="curve"/>
+    </>
   )
 }
 
