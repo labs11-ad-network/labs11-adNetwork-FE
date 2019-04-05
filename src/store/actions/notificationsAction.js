9fbb86da947e31190afe65919ca5b67102f0b8bd
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-04-05 15:41:22 -0400

    Hid all of keys in environment variables

--- src/store/actions/notificationsAction.js
+++ src/store/actions/notificationsAction.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = "https://lad-network.herokuapp.com";
+const URL = process.env.REACT_APP_BACKEND_URL;
 
 export const GET_USER_NOTIFICATIONS_START = "GET_USER_NOTIFICATIONS_START";
 export const GET_USER_NOTIFICATIONS_SUCCESS = "GET_USER_NOTIFICATIONS_SUCCESS";
@@ -14,6 +14,9 @@ export const getUserNotifications = () => dispatch => {
       dispatch({ type: GET_USER_NOTIFICATIONS_SUCCESS, payload: res.data });
     })
     .catch(err => {
-      dispatch({ type: GET_USER_NOTIFICATIONS_FAILURE, payload: err.response.data });
+      dispatch({
+        type: GET_USER_NOTIFICATIONS_FAILURE,
+        payload: err.response.data
+      });
     });
 };
