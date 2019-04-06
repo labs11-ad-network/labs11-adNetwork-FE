e8fd225e058acee0d4a6c05cec137c85f1659ef3
john2796 <jbmiranda22796@gmail.com>
2019-04-06 12:09:31 -0700

    added form just styling now

--- src/components/landing-page/Contact.js
+++ src/components/landing-page/Contact.js
@@ -1,17 +1,75 @@
 import React, { Component } from "react";
 import { ContactStyle } from "./ContactStyle";
 
+import contact1 from "../../assets/contact1.png";
+
 class Contact extends Component {
   render() {
     return (
       <ContactStyle>
         <div className="container">
           <div className="contact-title">
-            <h2>contact page here</h2>
+            <h2>Don’t Hasitate to Contact Us</h2>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. A non
-              mollitia velit fugiat ducimus.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              sollicitudin lobortis turpis, sit amet aliquet metus.
             </p>
+          </div>
+
+          <div className="form-flex">
+            <img src={contact1} alt="contact image  " />
+
+            <form className="contact-form">
+              <div className="row">
+                <div className="form-group">
+                  <input
+                    placeholder="First Name"
+                    id="fname"
+                    className="form-control"
+                    name="fname"
+                    type="text"
+                    required
+                    data-error="Please enter your first name"
+                  />
+                  <div className="help-block with-errors" />
+                </div>
+                <div className="form-group">
+                  <input
+                    placeholder="Last Name"
+                    id="lname"
+                    className="form-control"
+                    name="lname"
+                    type="text"
+                  />
+                </div>
+              </div>
+              <div className="form-group">
+                <input
+                  placeholder="Email Address"
+                  id="email"
+                  className="form-control"
+                  name="email"
+                  type="email"
+                  required
+                  data-error="Please enter your valid email address"
+                />
+              </div>
+              <div className="form-group">
+                <textarea
+                  placeholder="Your Comments"
+                  id="message"
+                  cols={20}
+                  rows={5}
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="buttons_container contact-btn">
+                <a href="/#" className="btn_blue">
+                  Learn More
+                </a>
+              </div>
+            </form>
           </div>
         </div>
       </ContactStyle>
--- src/components/landing-page/ContactStyle.js
+++ src/components/landing-page/ContactStyle.js
@@ -1,3 +1,58 @@
 import styled from "styled-components";
 
-export const ContactStyle = styled.div``;
+export const ContactStyle = styled.div`
+  .form-flex {
+    display: flex;
+    img {
+      max-width: 100%;
+      display: block;
+    }
+  }
+  form {
+    display: flex;
+    flex-wrap: wrap;
+    border: 1px solid red;
+  }
+  .row {
+    display: flex;
+  }
+  .form-control {
+    font-size: 16px;
+    color: #929292;
+    height: 54px;
+    border-radius: 27px;
+    border: 1px solid #dcdcdc;
+    padding: 5px 30px;
+    font-weight: 300;
+    display: block;
+    width: 100%;
+  }
+  .form-control #email {
+    flex: 1;
+  }
+  .contact-btn {
+    display: block;
+  }
+  textarea.form-control {
+    height: auto;
+  }
+  .form-group {
+    flex: auto;
+  }
+  textarea {
+    overflow: auto;
+    resize: vertical;
+    flex-direction: column;
+    cursor: text;
+    -webkit-appearance: textarea;
+    -webkit-rtl-ordering: logical;
+    font: 400 11px system-ui;
+    text-align: start;
+    word-spacing: normal;
+    text-transform: none;
+    text-indent: 0px;
+    text-shadow: none;
+    letter-spacing: normal;
+    text-rendering: auto;
+  }
+`;
