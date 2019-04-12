4bed169b22cb5d219a261e5ebcca8bf467623272
KieranVieira <kieranvieira@live.com>
2019-04-12 18:16:45 -0400

    Fixed user settings and changeUserData action creator

--- src/components/settings/tab-container/user-settings/UserSettingsForm.js
+++ src/components/settings/tab-container/user-settings/UserSettingsForm.js
@@ -6,24 +6,15 @@ import { Form } from "../../settingsStyles.js";
 
 class UserSettingsForm extends Component {
   state = {
-    userData: {
-      name: "",
-      nickname: "",
-      phone: "",
-      user_img: ""
-    }
+    userData: {}
   };
 
-  componentDidMount() {
-    const { name, nickname, phone } = this.props.currentUser;
-    this.setState({
-      userData: {
-        name,
-        nickname,
-        phone: phone || "",
-        user_img: ""
-      }
-    });
+  componentDidUpdate(prevProps) {
+    if (prevProps.currentUser !== this.props.currentUser) {
+      this.setState({
+        userData: this.props.currentUser
+      });
+    }
   }
 
   handleChange = e => {
@@ -47,7 +38,7 @@ class UserSettingsForm extends Component {
 
   changeUserData = e => {
     e.preventDefault();
-    this.props.changeUserData({...this.state.userData, show_tour: this.props.currentUser.show_tour});
+    this.props.changeUserData({...this.props.currentUser, ...this.state.userData});
   };
 
   render() {
